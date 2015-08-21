c269851a848d9b8315f983e609aaf90e0cabf06a
jdwilson <stlswengr@users.noreply.github.com>
2015-08-21 13:27:51 -0500

    wip, updating with EmployeeFactory and EmployeeFactoryTest classes.

--- src/main/java/com/jdw/clc/EmployeeFactory.java
+++ src/main/java/com/jdw/clc/EmployeeFactory.java
@@ -7,17 +7,23 @@ package com.jdw.clc;
 
 public class EmployeeFactory {
 
+    // tbd this may not be needed...
     Employee employee = new Employee(Employee.EmployeeRole.UNASSIGNED);
 
-//    private EmployeeFactory() {
-//        // tbd
-//    }
-//
-//    public static EmployeeFactory getInstance() {
-//        return new EmployeeFactory();
-//    }
+    private static final double initialManagerExpenseAllocation = 300.00;
+    private static final double initialDeveloperExpenseAllocation = 1000.00;
+    private static final double initialQATesterExpenseAllocation = 500.00;
 
-    public static Employee createEmployee(Employee.EmployeeRole role) {
-        return Employee(role);
+    public static Employee createEmployee(Employee.EmployeeRole role) throws EmployeeFactoryException {
+
+        if (role.equals(Employee.EmployeeRole.MANAGER)) {
+            return new Manager(initialManagerExpenseAllocation);
+        } else if (role.equals(Employee.EmployeeRole.DEVELOPER)) {
+            return new Employee(Employee.EmployeeRole.DEVELOPER, initialDeveloperExpenseAllocation);
+        } else if (role.equals(Employee.EmployeeRole.QA_TESTER)) {
+            return new Employee(Employee.EmployeeRole.QA_TESTER, initialQATesterExpenseAllocation);
+        } else {
+            throw new EmployeeFactoryException("Invalid Employee Role");
+        }
     }
 }
---
+++ src/main/java/com/jdw/clc/EmployeeFactoryException.java
@@ -0,0 +1,10 @@
+package com.jdw.clc;
+
+/**
+ * Created by jdwilson on 8/21/15.
+ */
+public class EmployeeFactoryException extends Exception {
+    public EmployeeFactoryException(String message) {
+        super(message);
+    }
+}
